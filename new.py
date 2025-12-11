22d303e0609fc6eb1ffa16fc372a46453db1bdd2
Lawarna Aree <supremaree@gmail.com>
2025-12-11 10:09:34 +0545

    This is the first commit for my python notes and codes

---
+++ new.py
@@ -0,0 +1,2 @@
+name = "Lawarna Aree"
+print(name)
---
+++ return_function.py
@@ -0,0 +1,11 @@
+def add():
+    a = 2
+    b = 3
+    total = a + b
+    return total
+
+def print_result():
+    result = add()
+    print(f" The result is {result}")
+
+print_result()
