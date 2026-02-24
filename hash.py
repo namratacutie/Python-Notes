edc909b920038f3c413895b67212f46e4eb90f66
Lawarna Aree <supremaree@gmail.com>
2026-02-24 15:10:08 +0545

    feat: Introduce error handling for hash table operations and add type comments to Record class attributes.

--- hash.py
+++ hash.py
@@ -1,43 +1,59 @@
 class Record:
     def __init__(self, Key, Data):
-        self.Key = Key
-        self.Data = Data
+        self.Key = Key #Integer
+        self.Data = Data #String
 
 HashTable = [[None for j in range(10)] for i in range(100)]
 
 def InitialiseHashTable():
     global HashTable
-    for i in range(100):
-        for j in range(10):
-            HashTable[i][j] = None
+
+    try:
+        for i in range(100):
+            for j in range(10):
+                HashTable[i][j] = None
+
+    except:
+        print("Error initializing hash table")
 
 def Hash(Key):
     return Key % 100
 
 def InsertData(Record):
     index = Hash(Record.Key)
-    for j in range(10):
-        if HashTable[index][j] is None:
-            HashTable[index][j] = Record
-            return
+    
+    try:
+        for j in range(10):
+            if HashTable[index][j] is None:
+                HashTable[index][j] = Record
+                return
+
+    except:
+        print("Error while inserting the data")
     
 def ReadData():
-    with open("HashTableData.txt", "r") as file:
-        for line in file:
-            parts = line.strip().split(",")
-            key = int(parts[0])
-            data = parts[1]
-            newRecord = Record(key, data)
-            InsertData(newRecord)
-        file.close()
+    try:
+        with open("HashTableData.txt", "r") as file:
+            for line in file:
+                parts = line.strip().split(",")
+                key = int(parts[0])
+                data = parts[1]
+                newRecord = Record(key, data)
+                InsertData(newRecord)
+            file.close()
+
+    except FileNotFoundError:
+        print("File not found")
 
 def GetRecord(Key):
     index = Hash(Key)
-    for j in range(10):
-        if HashTable[index][j] is not None:
-            if HashTable[index][j].Key == Key:
-                return HashTable[index][j].Data
-    return "Not found"
+    try:
+        for j in range(10):
+            if HashTable[index][j] is not None:
+                if HashTable[index][j].Key == Key:
+                    return HashTable[index][j].Data
+    except FileNotFoundError:
+        return "Not found"
 
 InitialiseHashTable()
 ReadData()
